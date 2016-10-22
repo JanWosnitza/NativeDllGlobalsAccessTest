71b5ce904d7e1d3b9b90b2be9ebbbae1b53ebbeb
JanWosnitza <jan_wosnitza@hotmail.com>
2016-10-22 13:14:25 +0200

    use median total time

--- NativeDllGlobalsAccessTest/Program.cs
+++ NativeDllGlobalsAccessTest/Program.cs
@@ -9,9 +9,31 @@ using System.Threading.Tasks;
 
 namespace NativeDllGlobalsAccessTest
 {
-    class Program
+    class Test
     {
-        static double Execute(string name, long count)
+        const long Count1 = 10000000;
+        const long Count2 = Count1 * 10;
+
+        public string Name;
+
+        private readonly List<double> Times1 = new List<double>();
+        private readonly List<double> Times2 = new List<double>();
+
+        public Test(string name)
+        {
+            this.Name = name;
+        }
+
+        public void TestOnce()
+        {
+            var time1 = Execute(Count1);
+            var time2 = Execute(Count2);
+
+            Times1.Add(time1);
+            Times2.Add(time2);
+        }
+
+        double Execute(long count)
         {
             var startInfo = new ProcessStartInfo();
             startInfo.CreateNoWindow = true;
@@ -19,7 +41,7 @@ namespace NativeDllGlobalsAccessTest
             startInfo.RedirectStandardInput = true;
 
             startInfo.UseShellExecute = false;
-            startInfo.Arguments = $"{name} {count}";
+            startInfo.Arguments = $"{this.Name} {count}";
             startInfo.FileName = "Tester.exe";
 
             using ( var process = new Process() )
@@ -41,30 +63,52 @@ namespace NativeDllGlobalsAccessTest
             }
         }
 
-        const long Count1 = 10000000;
-        const long Count2 = Count1 * 10;
-
-        static void Test(string name)
+        static double GetMedian(IEnumerable<double> seq)
         {
-            var time1 = Execute(name, Count1);
-            var time2 = Execute(name, Count2);
+            var arr = seq.OrderBy(x=>x).ToArray();
+            return arr[arr.Length / 2];
+        }
+
+        public void Print()
+        {
+            var time1 = GetMedian(this.Times1);
+            var time2 = GetMedian(this.Times2);
 
             var accessTime = (time1 - time2) / (Count1 - Count2);
             var initTime = time2 - accessTime * Count2;
 
-            Console.WriteLine($"{name}: total-time: {time1+time2}, access-time = {accessTime}, initTime = {initTime}");
+            Console.WriteLine($"{this.Name}:");
+            Console.WriteLine($"    total-time  = {time1 + time2}");
+            Console.WriteLine($"    access-time = {accessTime}");
+            Console.WriteLine($"    initTime    = {initTime}");
         }
+    }
 
+    class Program
+    {
         static void Main(string[] args)
         {
             // init
             //Execute<LazyInit>(10);
 
-            // test
-            Test("LazyInit");
-            Test("LazyInitInterlocked");
-            Test("StaticInitProperty");
-            Test("StaticInitField");
+            var testNames = new []{ "LazyInit", "LazyInitInterlocked", "StaticInitProperty", "StaticInitField" };
+            var tests = testNames.Select(x => new Test(x)).ToArray();
+
+            var testRange =
+                Enumerable.Range(0, 10)
+                    .SelectMany(x => tests)
+                    .Shuffle()
+                    .ToArray();
+
+            for ( var i = 0; i < testRange.Length; i++ )
+            {
+                Console.Write($"\rTest {i + 1}/{testRange.Length}");
+                testRange[i].TestOnce();
+            }
+            Console.WriteLine();
+
+            foreach ( var test in tests )
+                test.Print();
         }
     }
 }
